4cc64111a852835219d0642076a192708d082df9
Ananthesh J Shet <shetanantheshnlsv@yahoo.in>
2024-10-07 09:21:05 +0530

    Updates test case

--- test_flask_app.py
+++ test_flask_app.py
@@ -6,6 +6,40 @@ import json
 def client():
     return app.test_client()
 
-def test_home(client):
-    resp = client.get('/')
+def test_predict_high_probability(client):
+    # Provide form data manually
+    form_data = {'GRE Score': '318.0',
+                 'TOEFL Score': '106.0',
+                 'University Rating': '2.0',
+                 'SOP': '4.0',
+                 'LOR': '4.0',
+                 'CGPA': '7.92',
+                 'Research': '1.0'}
+
+    # Simulate POST request with form data
+    resp = client.get('/predict', data=form_data)
+
+    # Assert the response is 200 OK
     assert resp.status_code == 200
+
+    # Check if the correct message is rendered
+    assert b"Admission probability is high: 0.66" in resp.data
+
+def test_predict_low_probability(client):
+    # Provide form data manually
+    form_data = {'GRE Score': '100.0',
+                 'TOEFL Score': '40.0',
+                 'University Rating': '2.0',
+                 'SOP': '2.0',
+                 'LOR': '2.0',
+                 'CGPA': '4.5',
+                 'Research': '0.0'}
+
+    # Simulate POST request with form data
+    resp = client.get('/predict', data=form_data)
+
+    # Assert the response is 200 OK
+    assert resp.status_code == 200
+
+    # Check if the correct message is rendered
+    assert b"Admission probability is low: -0.45" in resp.data
